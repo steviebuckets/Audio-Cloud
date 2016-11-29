f37f72925360541c4a915feb77276c36f677354e
steviebuckets <steviebuckets33@gmail.com>
2016-11-29 11:45:52 -0800

    cleaned up code and updated logic

--- app.js
+++ app.js
@@ -1,54 +1,48 @@
+/* this should hide the search input on submit and display the results section with embedded player */
 $(document).ready(function() {
     $('form').submit(function(e) {
         e.preventDefault();
         getDataFromApi($('#inputSuccess5').val());
         $('header').hide();
         $('#results').show();
-
-
     });
 });
 
-
+/*this logs the keydown in the input form and returns the value to the form */
+/*this also assigns a minimum value to the char before the callback/searc to api data begins */
 var musicSearch = "";
 $("#user-search").keydown(function(event) {
 
     if (musicSearch.length > 3) {
         userMixcloudSearch();
+        musicSearch += String.fromCharCode(event.which);
     }
-    musicSearch += String.fromCharCode(event.which);
 
-
-
+    /*this logs the keydown in the input form if the user hits the delete key/*
+    /*after the delete keydown is logged it refreshes the value in the input form to refelect the new string*/
 }).keyup(function(event) {
     if (event.which == 8) {
         musicSearch = $('#user-search').val();
-
     }
-
-    userMixcloudSearch();
-
-
-
+    //userMixcloudSearch();
 });
 
-
-
+/*Callback execution, when function is invoked input value from search is now processed and searched*/
 function userMixcloudSearch() {
     var options = {
-        data: []
-    }
+            data: []
+        }
+        /*API callback and URL, gets user DATA*/
     $.getJSON("https://api.mixcloud.com/search/", { //old api "https://api.mixcloud.com/search?callback=?", {
-            //are these the right parameters for the object?
             type: 'user',
             q: musicSearch
         })
         .done(function(response) {
 
-            //var username = response.data[0].username;
-            //var thumb = response.data[0].pictures.small;
-            //var kind = response.data[0].name;      
-            //console.log(username);
+            var username = response.data[0].username;
+            var thumb = response.data[0].pictures.small;
+            var kind = response.data[0].name;
+            console.log(username);
 
             var data = []
             for (var i = 0; i < response.data.length; i++) {
@@ -57,98 +51,64 @@ function userMixcloudSearch() {
 
             var options = {
                 data: data,
-                requestDelay: 300,
+                requestDelay: 200,
                 getValue: "name",
 
-
-
-
-                //[{ name: (username), type: (name), icon: (thumb) }],
-                //getValue: "name",
-
-
+                /*easyAutoComplete template for displaying data in input form*/
                 template: {
                     type: "custom",
                     method: function(value, item) {
-                        return "<img src='" + item.icon + "' /> | " + value;
+                        return "<img src='" + item.icon + "' />  " + value;
                     }
                 }
             };
 
-            // add name to the data array in the options objects (fancy terms)
-            // hint: loop through the data in the response object.
+            /*input value from search sent to easyAutoComplete*/
             $("#user-search").easyAutocomplete(options);
-
-
-
-        });
-
-}
-
-
-//function getDataFromApi(query) {
-//is this the right callback function?
-
-// step 1: get the user
-/* $.getJSON("https://api.mixcloud.com/search?callback=?", {
-            //are these the right parameters for the object?
-            type: 'user',
-            q: query
         })
-        .done(
-            function(response) {
 
-                // only one user is returned (0 index)
-                // pros: - very easy
-                // cons: - lose the person you are looking for
-                var username = response.data[0].username;
-                // step 2: get the user's meta data
-                $.getJSON("https://api.mixcloud.com/" + username + "?callback=?", {
-                        metadata: "1"
-                    })
-                    .done(function(response) {
-                        var url = response.metadata.connections.cloudcasts;
-                        // step 3: gets the cloudcasts  
-                        $.getJSON(url + "?callback=?", {
-                                limit: 2
-                            })
-                            .done(function(response) {
-                                displayAPIResults(response.data);
-                            });
-                    });
-
-            });
-
-//}
-
-/*var options = {
-	data: [ {name: "username, type: "air", icon: "http://l},
-		{name: username, type: "ground", icon: thumbnail},
-		{name: username, type: "ground", icon: "http://lorempixel.com/100/50/transport/10"},
-		{name: username, type: "air", icon: "http://lorempixel.com/100/50/transport/1"},
-		{name: username, type: "ground", icon: "http://lorempixel.com/100/50/transport/6"}],
+    }
 
 
-	getValue: "name",
 
-	template: {
-		type: "custom",
-		method: function(value, item) {
-			return "<img src='" + item.icon + "' /> | " + item.type + " | " + value;
-		}
-	}
-};
 
-$("#template-custom").easyAutocomplete(options);
+    /*Original logic for searching API and displaying to DOM
+    $.getJSON("https://api.mixcloud.com/search?callback=?", {
+                //are these the right parameters for the object?
+                type: 'user',
+                q: query
+            })
+            .done(
+                function(response) {
 
-*/
-function displayAPIResults(uploads) {
-    $('#results').empty();
-    var html = "";
+                    
+                    var username = response.data[0].username;
+                    
+                    $.getJSON("https://api.mixcloud.com/" + username + "?callback=?", {
+                            metadata: "1"
+                        })
+                        .done(function(response) {
+                            var url = response.metadata.connections.cloudcasts;
+                            // step 3: gets the cloudcasts  
+                            $.getJSON(url + "?callback=?", {
+                                    limit: 2
+                                })
+                                .done(function(response) {
+                                    displayAPIResults(response.data);
+                                });
+                        });
 
-    $.each(uploads, function(i, upload) {
+                });
+
+    }
+
+        function displayAPIResults(uploads) {
+       $('#results').empty();
+        var html = "";
+
+        $.each(uploads, function(i, upload) {
         var audioPlayer = "https://api.mixcloud.com/" + upload.key + "/embed-html";
         html = html + "<li>" + "<iframe width='100%' height='120%' style='color: rgb(58, 58, 61)' frameborder='0' src='" + audioPlayer + "'></iframe>" + "<li>"
-    });
-    $('#results').html(html);
-}
+        });
+      $('#results').html(html);
+    */
